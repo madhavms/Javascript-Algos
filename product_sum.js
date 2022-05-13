95bac73de57d41e88fc3bf559cf29c94c040adff
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-05-13 15:04:57 +0100

    Create product_sum.js

---
+++ product_sum.js
@@ -0,0 +1,20 @@
+function productSum(array, multiplier=1) {
+    // Write your code here.
+		let sum = 0;
+    for(const element of array){
+			if(Array.isArray(element)) {
+				sum += productSum(element, multiplier+1);
+			}
+      else{
+        sum += element;
+          }
+      }
+      return sum*multiplier; 
+  }
+
+let array = [5, 2, [7, -1], 3, [6, [-13, 8], 4]];
+console.log(productSum(array));
+
+/*
+5 + 2 + 2 * (7 - 1) + 3 + 2 * (6 + 3 * (-13 + 8) + 4) = 12
+*/
