240dbe7555f50eaf5a57763e8d9ee3126a5a01fd
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-10-31 23:42:01 +0000

    Update product_sum.js

--- product_sum.js
+++ product_sum.js
@@ -1,16 +1,15 @@
-function productSum(array, multiplier=1) {
-    // Write your code here.
-		let sum = 0;
-    for(const element of array){
-			if(Array.isArray(element)) {
-				sum += productSum(element, multiplier+1);
-			}
-      else{
-        sum += element;
-          }
-      }
-      return sum*multiplier; 
+function productSum(array, multiplier = 1) {
+  // Write your code here.
+  let sum = 0;
+  for (const element of array) {
+    if (Array.isArray(element)) {
+      sum += productSum(element, multiplier + 1);
+    } else {
+      sum += element;
+    }
   }
+  return sum * multiplier;
+}
 
 let array = [5, 2, [7, -1], 3, [6, [-13, 8], 4]];
 console.log(productSum(array));
