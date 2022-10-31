ff90fe0a3d3ce5663d7f924789fa35d754b302dc
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-10-31 23:42:20 +0000

    Update valid_paranthesis.js

--- valid_paranthesis.js
+++ valid_paranthesis.js
@@ -1,28 +1,25 @@
-var isValid = function(s) {
-    
-    let stack = [];
+var isValid = function (s) {
+  let stack = [];
 
-    for(let c of s){
-        console.log(c);
-        switch(c){
-            case '{':
-                stack.push('}');
-                break
-            case '(':
-                stack.push(')');
-                break
-            case '[':
-                stack.push(']');
-                break
-            
-            default:
-                if(c!== stack.pop()) return false;
-                
-                
-        }
+  for (let c of s) {
+    console.log(c);
+    switch (c) {
+      case "{":
+        stack.push("}");
+        break;
+      case "(":
+        stack.push(")");
+        break;
+      case "[":
+        stack.push("]");
+        break;
+
+      default:
+        if (c !== stack.pop()) return false;
     }
-    
-    return true;
+  }
+
+  return true;
 };
 
-console.log(isValid('['));
+console.log(isValid("["));
