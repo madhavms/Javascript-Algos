7a29d257303cfa15b1eeb9adeeae9b6ee1dedeb0
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-11-03 20:33:48 +0000

    Update removeElement.js

--- removeElement.js
+++ removeElement.js
@@ -1,14 +1,14 @@
-var removeElement = function(nums, val) {
+var removeElement = function (nums, val) {
     let count = 0;
-    for(let idx = 0; idx < nums.length; idx++){
-        if(nums[idx] == val) count ++;
-        else nums[idx - count] = nums[idx];    
+    for (let idx = 0; idx < nums.length; idx++) {
+        if (nums[idx] == val) count++;
+        else nums[idx - count] = nums[idx];
     }
     console.log(nums);
     return count;
 };
 
-let nums = [0,1,2,2,3,0,4,2];
+let nums = [0, 1, 2, 2, 3, 0, 4, 2];
 let val = 2;
 
 console.log(removeElement(nums, val));
