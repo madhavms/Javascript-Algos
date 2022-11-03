f00ddf76b51f3aa4bf273bf3afc823dca7a24a8a
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-11-03 20:33:52 +0000

    Update sortedsquares.js

--- sortedsquares.js
+++ sortedsquares.js
@@ -1,18 +1,17 @@
 
 function sortedSquaredArray(array) {
-    // Write your code here.
-      const sortedSquares = new Array(array.length).fill(0);
-      
-      for (let idx = 0; idx < array.length; idx ++){
-          const value = array[idx];
-          sortedSquares[idx] = value * value;
-      }
-      
-      sortedSquares.sort((a,b) => a - b);
-  
-    return sortedSquares;
+  // Write your code here.
+  const sortedSquares = new Array(array.length).fill(0);
+
+  for (let idx = 0; idx < array.length; idx++) {
+    const value = array[idx];
+    sortedSquares[idx] = value * value;
   }
-  
-  // Do not edit the line below.
-  exports.sortedSquaredArray = sortedSquaredArray;
-  
+
+  sortedSquares.sort((a, b) => a - b);
+
+  return sortedSquares;
+}
+
+// Do not edit the line below.
+exports.sortedSquaredArray = sortedSquaredArray;
