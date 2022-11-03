4437ac9a27e04b4c0e601b016b0177e31e2aaa1f
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-11-03 20:33:42 +0000

    Update mergeSortedArray.js

--- mergeSortedArray.js
+++ mergeSortedArray.js
@@ -1,13 +1,13 @@
-var merge = function(nums1, m, nums2, n) {  
-    for(let idx = m+n-1; idx > m-1; idx --){
+var merge = function (nums1, m, nums2, n) {
+    for (let idx = m + n - 1; idx > m - 1; idx--) {
         nums1[idx] = nums2[m + n - idx - 1];
     }
-    return nums1.sort((a,b) => a-b);
+    return nums1.sort((a, b) => a - b);
 };
 
-nums1 = [1,2,3,0,0,0];
+nums1 = [1, 2, 3, 0, 0, 0];
 m = 3;
-nums2 = [2,5,6];
+nums2 = [2, 5, 6];
 n = 3;
 
 console.log(merge(nums1, m, nums2, n));
