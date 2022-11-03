e25b1330d1ae36580a8d6e8191f5dde7de8e37ee
Madhav Manohar S <madhavmanohar.s@gmail.com>
2022-11-03 20:33:45 +0000

    Update priority_sort.js

--- priority_sort.js
+++ priority_sort.js
@@ -1,17 +1,17 @@
-function dataObject(priority, value){
+function dataObject(priority, value) {
     this.priority = priority;
     this.value = value;
 }
 
-function generateRadomObject(n){
+function generateRadomObject(n) {
     let list = [undefined]
-    for(let idx = 0; idx < 10; idx ++){
+    for (let idx = 0; idx < 10; idx++) {
         let num = Math.floor((Math.random() * 100) + 1);
         list.push(num);
-        if(num % 2 == 0) list.push(undefined);
+        if (num % 2 == 0) list.push(undefined);
     }
 
-    objList = list.map(num => new dataObject(num, 'data_'+num));
+    objList = list.map(num => new dataObject(num, 'data_' + num));
 
     return objList;
 }
@@ -20,18 +20,19 @@ const objArray = generateRadomObject(10);
 
 function sortObjectArray(objArray) {
 
-    objArray = objArray.sort((a,b) => {
+    objArray = objArray.sort((a, b) => {
         a_priority = a.priority || 0;
         b_priority = b.priority || 0;
-        return a_priority > b_priority ? 1 : -1});
+        return a_priority > b_priority ? 1 : -1
+    });
 
     return objArray;
 }
 
 console.log('\nORIGINAL ARRAY:');
-console.log(objArray,'\n');
+console.log(objArray, '\n');
 
 let sortedArray = sortObjectArray(objArray);
 
 console.log('\SORTED ARRAY:');
-console.log(sortedArray,'\n');
+console.log(sortedArray, '\n');
